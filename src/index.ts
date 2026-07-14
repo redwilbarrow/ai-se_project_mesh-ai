dcd0ac8700dec5aae5216a20f38015500462a3d5
RedWilBarrow <pwilkerson513@gmail.com>
2026-07-14 07:13:32 -0500

    build: incorporated env var instead of hard coded

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import express from 'express';
 import router from './routes/index.js';
 import { logger } from './middleware/logger.js';
 import { errorHandler, notFoundHandler } from './middleware/error.js';
 
 const app = express();
+const port = process.env.PORT || 3000;
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(logger);
@@ -18,7 +23,6 @@ app.use(router);
 app.use(notFoundHandler);
 app.use(errorHandler);
 
-const port = 3000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
