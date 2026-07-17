ff9c3fd28216f73b0ca9f7e68731951bb2d96a4f
RedWilBarrow <pwilkerson513@gmail.com>
2026-07-17 07:36:03 -0500

    feat: replace messageChat with createMessage for chat message handling

--- src/routes/chats.ts
+++ src/routes/chats.ts
@@ -4,8 +4,8 @@ import {
   createChat,
   getChat,
   deleteChat,
-  messageChat,
 } from '../controllers/chats.js';
+import { createMessage } from '../controllers/messages.js';
 import { auth } from '../middleware/auth.js';
 
 const chatRouter = Router();
@@ -14,6 +14,6 @@ chatRouter.get('/', auth, getChats);
 chatRouter.post('/', auth, createChat);
 chatRouter.get('/:id', auth, getChat);
 chatRouter.delete('/:id', auth, deleteChat);
-chatRouter.post('/:id/messages', auth, messageChat);
+chatRouter.post('/:id/messages', auth, createMessage);
 
 export { chatRouter };
