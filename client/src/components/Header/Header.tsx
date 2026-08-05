9b14e10ee5a87033c20f60562f56d5ad8525bf54
RedWilBarrow <pwilkerson513@gmail.com>
2026-08-05 06:08:19 -0500

    feat: add logo images and implement Header component with navigation links

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,11 +1,25 @@
+import logo from "../../assets/images/logo-2.png";
 import "./Header.css";
+import { NavLink } from "react-router-dom";
 
 export default function Header() {
+  function getNavLinkClass({ isActive }: { isActive: boolean }) {
+    return isActive
+      ? "header__nav-link header__nav-link_active"
+      : "header__nav-link";
+  }
+
   return (
     <header className="header">
-      <div className="header__inner">
-        <h1 className="header__title">Mesh-AI</h1>
-      </div>
+      <img src={logo} alt="MeshAI logo" className="header__logo" />
+      <nav className="header__nav">
+        <NavLink to="/knowledge" className={getNavLinkClass}>
+          Knowledge Base
+        </NavLink>
+        <NavLink to="/chat" className={getNavLinkClass}>
+          Chat
+        </NavLink>
+      </nav>
     </header>
   );
 }
